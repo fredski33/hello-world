78ae18b8d90f98df51db5d14aab6d0bbc8e2378e
fredski33 <thinkb4sink@gmail.com>
2019-02-02 19:17:08 +0100

    Let's start

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header.js"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby the great Motherfucker!">
+    <p>Such wow! Very React.</p>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+  <h1>Hi! Fuck you Gatsby!</h1>
+  <p>What a world isn't it?</p>
+  <img src="https://source.unsplash.com/random/400x200" alt=""/>
+  </div>
+)
